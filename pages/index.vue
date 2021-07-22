9b85c88a71138ed8b66fc2ef0c8e6e954202556e
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-07-22 14:10:31 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>
+      <Navbar/>
+      <section class="w-3/4 mx-auto mt-28 flex justify-between items-center">
+        <div class="w-2/5 items-center">
+          <h1 class="font-bold text-5xl leading-snug">Siap Kurban <br> Tahun Depan!</h1>
+          <p class="text-xl mt-5 mb-4">Program Nabung 10.000 Rupiah, Siapapun Berkenan Berkurban</p>
+          <button class="px-4 py-2 bg-yellow-400 rounded-lg font-semibold">Mulai Nabung, Gratis!</button>
+        </div>
+        <div class="w-1/2">
+          <img src="image/cow.png" width="700" alt="sapi kurban">
+        </div>
+      </section>
+      <section class="mt-20">
+        <h1 class="font-semibold text-sm uppercase text-center">Telah dipercaya Oleh Beberapa Perusahaan Ternama</h1>
+          <div class="flex justify-center py-6">
+            <div class="grid grid-flow-col gap-6">
+              <img src="/vendor/ovo.png" width="80" alt="ovo">
+              <img src="/vendor/kitabisacom.png" width="120" alt="kitabisa">
+              <img src="/vendor/jenius.png" width="100" alt="jenius">
+              <img src="/vendor/gojek.png" width="100" alt="gojek">
+            </div>
+          </div>
+       
+      </section>
+      <section class="bg-yellow-100 mt-20">
+        <div class="w-3/4 mx-auto py-12">
+          <div class="flex justify-center space-x-4">
+              <img src="https://avatars.githubusercontent.com/u/17506770?v=4" width="150" class="rounded-xl" alt="">
+              <div class="h-20 w-2/5 space-y-5">
+                <p class="font-semibold text-xl">
+                  Qurbanku Membantu Saya dan Rekan-Rekan Saya Untuk Patungan Sapi Untuk Qurban Dari Nabung 10.000 Perhari, Dan itu Luar Biasa. 
+                </p>
+                <p class="uppercase text-sm"><span class="font-medium">— David Tippett,</span> Karyawan OVO</p>
+              </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="w-3/4 mx-auto mt-24">
+        <h1 class="text-center text-3xl font-bold">Qurbanku Juga Menyediakan Akiqah Murah</h1>
+      </section>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+body{
+  font-family: 'Poppins', sans-serif;
+}
+</style>
+
+<script>
+export default {}
+</script>
