69d4c41bb56cce4e3de1ef462bb60e5f096d0262
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-07-24 18:45:20 +0700

    finishing

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,49 @@
           </div>
        
       </section>
+
+      <section class="mt-20">
+        <div class="w-3/4 mx-auto">
+          <h1 class="text-center text-3xl font-semibold">Daftar Paket</h1>
+          <div class="flex justify-center items-center space-x-10 my-12">
+            <div class="w-1/4 bg-yellow-100 py-4 px-6 rounded -mt-10">
+              <div class="text-xs pb-3 text-center">Rekomendasi</div>
+              <h2 class="text-center font-semibold h-full rounded pb-3">Qurban Standar</h2>
+              <p class="text-center text-2xl font-bold">Rp 1.850.000</p>
+              <ul class="mt-5">
+                <li>Kambing / Domba</li>
+                <li>Berat 18-20 kg</li>
+              </ul>
+              <div class="mt-4">
+                <button class="px-4 py-2 rounded-md font-semibold text-center bg-yellow-400 w-full text-sm">Lihat Selengkapnya</button>
+              </div>
+            </div>
+            <div class="w-1/4 bg-gray-100 py-4 px-6 rounded">
+              <h2 class="text-center font-semibold h-full rounded pb-3">1/7 Sapi</h2>
+              <p class="text-center text-2xl font-bold">Rp 2.350.000</p>
+              <ul class="mt-5">
+                <li>Sapi</li>
+                <li>Berat 210-230 kg</li>
+              </ul>
+              <div class="mt-4">
+                <button class="px-4 py-2 rounded-md font-semibold text-center bg-yellow-400 w-full text-sm">Lihat Selengkapnya</button>
+              </div>
+            </div>
+            <div class="w-1/4 bg-gray-100 py-4 px-6 rounded">
+              <h2 class="text-center font-semibold h-full rounded pb-3">Sapi</h2>
+              <p class="text-center text-2xl font-bold">Rp 16.000.000</p>
+              <ul class="mt-5">
+                <li>Sapi</li>
+                <li>Berat 200-230 kg</li>
+              </ul>
+              <div class="mt-4">
+                <button class="px-4 py-2 rounded-md font-semibold text-center bg-yellow-400 w-full text-sm">Lihat Selengkapnya</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section class="bg-yellow-100 mt-20">
         <div class="w-3/4 mx-auto py-12">
           <div class="flex justify-center space-x-4">
@@ -39,7 +82,82 @@
       </section>
 
       <section class="w-3/4 mx-auto mt-24">
-        <h1 class="text-center text-3xl font-bold">Qurbanku Juga Menyediakan Akiqah Murah</h1>
+        <h1 class="text-center text-3xl font-bold">Qurbanku Juga Menyediakan Aqiqah</h1>
+        <div class="flex justify-center items-center space-x-10 my-16">
+          <div class="w-1/4 bg-gray-100 py-4 px-6 rounded">
+            <h2 class="text-center font-semibold h-full rounded pb-3">Aqiqah Standar</h2>
+            <p class="text-center text-2xl font-bold">Rp 2.300.000</p>
+            <ul class="mt-5">
+              <li>250 Tusuk Sate</li>
+              <li>80 Gule</li>
+            </ul>
+            <div class="mt-4">
+              <button class="px-4 py-2 rounded-md font-semibold text-center bg-yellow-400 w-full text-sm">Lihat Selengkapnya</button>
+            </div>
+          </div>
+          <div class="w-1/4 bg-yellow-100 py-4 px-6 rounded -mt-5">
+          <div class="text-xs pb-3 text-center">Rekomendasi</div>
+            <h2 class="text-center font-semibold h-full rounded pb-3">Aqiqah Super</h2>
+            <p class="text-center text-2xl font-bold">Rp 2.400.000</p>
+            <ul class="mt-5">
+              <li>300 Tusuk Sate</li>
+              <li>100 Gule</li>
+            </ul>
+            <div class="mt-4">
+              <button class="px-4 py-2 rounded-md font-semibold text-center bg-yellow-400 w-full text-sm">Lihat Selengkapnya</button>
+            </div>
+          </div>
+          <div class="w-1/4 bg-gray-100 py-4 px-6 rounded">
+            <h2 class="text-center font-semibold h-full rounded pb-3">Aqiqah Istimewa</h2>
+            <p class="text-center text-2xl font-bold">Rp 3.150.000</p>
+            <ul class="mt-5">
+              <li>450 Tusuk Sate</li>
+              <li>140 Gule</li>
+            </ul>
+            <div class="mt-4">
+              <button class="px-4 py-2 rounded-md font-semibold text-center bg-yellow-400 w-full text-sm">Lihat Selengkapnya</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="mt-52 pb-5 w-3/4 mx-auto">
+        <div class="flex justify-between">
+          <div class="w-1/4">
+            <h1 class="text-2xl font-bold">Qurbanku</h1>
+            <p class="mt-4">
+              Qurbanku adalah platform untuk menabung qurban sekaligus marketplace untuk membeli langsung qurban dan aqiqah
+            </p>
+            <p class="mt-3">
+              &copy; 2021 Qurbanku
+            </p>
+          </div>
+          <div class="w-3/4 flex justify-end space-x-10">
+            <div>
+              <h2 class="font-semibold text-xl">Produk</h2>
+              <ul class="mt-3 space-y-2">
+                <li class="hover:underline"><a href="#">Nabung Qurban</a></li>
+                <li class="hover:underline"><a href="#">Qurban</a></li>
+                <li class="hover:underline"><a href="#">Aqiqah</a></li>
+              </ul>
+            </div>
+            <div>
+              <h2 class="font-semibold text-xl">Perusahaan</h2>
+              <ul class="mt-3 space-y-2">
+                <li class="hover:underline"><a href="#">Syarat & Ketentuan</a></li>
+                <li class="hover:underline"><a href="#">Kebijakan Privasi</a></li>
+                <li class="hover:underline"><a href="#">FAQ</a></li>
+              </ul>
+            </div>
+            <div>
+              <h2 class="font-semibold text-xl">Sosial Media</h2>
+              <ul class="mt-3 space-y-2">
+                <li class="hover:underline"><a href="#">Facebook</a></li>
+                <li class="hover:underline"><a href="#">Instagram</a></li>
+                <li class="hover:underline"><a href="#">Youtube</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   </div>
@@ -50,6 +168,7 @@
 body{
   font-family: 'Poppins', sans-serif;
 }
+
 </style>
 
 <script>
