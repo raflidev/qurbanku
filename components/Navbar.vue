33836279a3104d566fc3889554999eee784f54ed
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-07-24 18:54:31 +0700

    finishing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,8 +2,8 @@
   <div class="w-3/4 mx-auto flex justify-between items-center pt-7">
     <h1 class="text-2xl font-bold">Qurbanku</h1>
     <div class="space-x-5">
-      <router-link to="/auth/login" class="hover:underline p-3 rounded">Login</router-link>
-      <router-link to="/auth/register" class="text-yellow-700 underline hover:no-underline p-3 rounded">Register</router-link>
+      <router-link to="#" class="hover:underline p-3 rounded">Login</router-link>
+      <router-link to="#" class="text-yellow-700 underline hover:no-underline p-3 rounded">Register</router-link>
     </div>
 </div>
 </template>
